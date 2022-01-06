0a4abea93360dff9e1ad28b57ed78b85bab82539
INoTime <niklas.lurse@icloud.com>
2022-01-06 11:17:53 +0100

    changed year

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 /*
  * This file is part of contao-release-stages-bundle.
- * 
- * (c) BROCKHAUS AG 2021 <info@brockhaus-ag.de>
+ *
+ * (c) BROCKHAUS AG 2022 <info@brockhaus-ag.de>
  * @license GPL-3.0-or-later
  * For the full copyright and license information,
  * please view the LICENSE file that was distributed with this source code.
