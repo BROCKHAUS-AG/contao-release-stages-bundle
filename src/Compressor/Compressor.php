d81dbd9ef517c545c1963e06dd03e3ccd10f6901
INoTime <niklas.lurse@icloud.com>
2022-04-01 15:22:19 +0200

    added compressor to compress files

---
+++ src/Compressor/Compressor.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of contao-release-stages-bundle.
+ *
+ * (c) BROCKHAUS AG 2022 <info@brockhaus-ag.de>
+ * @license GPL-3.0-or-later
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/brockhaus-ag/contao-release-stages-bundle
+ */
+
+namespace BrockhausAg\ContaoReleaseStagesBundle\Compressor;
+
+use BrockhausAg\ContaoReleaseStagesBundle\Logger\Log;
+use Exception;
+use Phar;
+use PharData;
+
+class Compressor {
+    private Log $_log;
+
+    public function __construct(Log $log)
+    {
+        $this->_log = $log;
+    }
+
+    public function compress(array $files, string $compressedFile): void
+    {
+        try {
+            $archive = new PharData($compressedFile);
+            foreach ($files as $file) {
+                $archive->addFile($file);
+            }
+            $archive->compress(Phar::GZ);
+            $this->_log->info("Successfully saved ". $compressedFile);
+        }catch (Exception $e) {
+            $this->_log->logErrorAndDie($e->getMessage());
+        }
+    }
+}
