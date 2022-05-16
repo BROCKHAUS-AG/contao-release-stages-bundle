26590ef9f96a1b16647cdef8e41ed9b4cd405421
INoTime <niklas.lurse@icloud.com>
2022-05-16 14:02:23 +0200

    changed buttons for dca

---
+++ src/EventListener/DataContainer/EditButtonsCallback.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of contao-release-stages-bundle.
+ *
+ * (c) BROCKHAUS AG 2022 <info@brockhaus-ag.de>
+ * @license GPL-3.0-or-later
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/brockhaus-ag/contao-release-stages-bundle
+ */
+
+namespace BrockhausAg\ContaoReleaseStagesBundle\EventListener\DataContainer;
+
+use Contao\CoreBundle\ServiceAnnotation\Callback;
+use Contao\DataContainer;
+
+/**
+ * @Callback(table="tl_release_stages", target="edit.buttons")
+ */
+class EditButtonsCallback
+{
+    public function __invoke(array $buttons, DataContainer $dc): array
+    {
+        unset($buttons['save']);
+        unset($buttons['saveNcreate']);
+        unset($buttons['saveNduplicate']);
+
+        return $buttons;
+    }
+}
