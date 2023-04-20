7efe65acb810e3bc582537f1fbb9e73634f2157d
ysommer <yves0sommer@gmail.com>
2023-04-20 12:20:27 +0200

    fixed: broken tests in ConfigMapperTest.php

--- tests/BrockhausAg/ContaoReleaseStagesBundle/Tests/Mapper/ConfigMapperTest.php
+++ tests/BrockhausAg/ContaoReleaseStagesBundle/Tests/Mapper/ConfigMapperTest.php
@@ -41,11 +41,22 @@ class ConfigMapperTest extends ContaoTestCase
     public function testMap(): void
     {
         $input = '{
-              "database": {
+              "databaseProd": {
                 "server": "192.168.0.2",
                 "name": "prodContao",
                 "port": 3306,
                 "username": "prodContao",
+                "password": "admin1234",
+                "ignoredTables": [
+                  "tl_to_be_ignored",
+                  "tl_to_be_ignored_too"
+                ]
+              },
+              "databaseTest": {
+                "server": "hier-kommen-die-daten-her",
+                "name": "testContao",
+                "port": 3306,
+                "username": "admin",
                 "password": "admin1234",
                 "ignoredTables": [
                   "tl_to_be_ignored",
@@ -81,7 +92,9 @@ class ConfigMapperTest extends ContaoTestCase
             }
         ';
 
-        $expectedDatabase = new Database("192.168.0.2", "prodContao", 3306, "prodContao",
+        $expectedProdDatabase = new Database("192.168.0.2", "prodContao", 3306, "prodContao",
+            "admin1234", array("tl_to_be_ignored", "tl_to_be_ignored_too"));
+        $expectedTestDatabase = new Database("hier-kommen-die-daten-her", "testContao", 3306, "admin",
             "admin1234", array("tl_to_be_ignored", "tl_to_be_ignored_too"));
         $expectedFtp = new Ftp(1234, "admin", "admin1234", false);
         $expectedSsh = new Ssh(1234, "admin", "admin1234");
@@ -89,18 +102,24 @@ class ConfigMapperTest extends ContaoTestCase
         $expectedDNSRecords = new DNSRecordCollection();
         $expectedDNSRecords->add(new DNSRecord("example-site", "www.example-site.de"));
         $expectedDNSRecords->add(new DNSRecord("example-site-better", "www.example-site-better.de"));
-        $expected = new Config($expectedDatabase, $expectedFileServer, 0,
+        $expected = new Config($expectedProdDatabase, $expectedTestDatabase, $expectedFileServer, 0,
             $expectedDNSRecords);
         $mapper = new ConfigMapper();
 
         $actual = $mapper->map(json_decode($input));
 
-        self::assertSame($expectedDatabase->getServer(), $actual->getProdDatabase()->getServer());
-        self::assertSame($expectedDatabase->getName(), $actual->getProdDatabase()->getName());
-        self::assertSame($expectedDatabase->getPort(), $actual->getProdDatabase()->getPort());
-        self::assertSame($expectedDatabase->getUsername(), $actual->getProdDatabase()->getUsername());
-        self::assertSame($expectedDatabase->getPassword(), $actual->getProdDatabase()->getPassword());
-        self::assertSame($expectedDatabase->getIgnoredTables(), $actual->getProdDatabase()->getIgnoredTables());
+        self::assertSame($expectedProdDatabase->getServer(), $actual->getProdDatabase()->getServer());
+        self::assertSame($expectedProdDatabase->getName(), $actual->getProdDatabase()->getName());
+        self::assertSame($expectedProdDatabase->getPort(), $actual->getProdDatabase()->getPort());
+        self::assertSame($expectedProdDatabase->getUsername(), $actual->getProdDatabase()->getUsername());
+        self::assertSame($expectedProdDatabase->getPassword(), $actual->getProdDatabase()->getPassword());
+        self::assertSame($expectedProdDatabase->getIgnoredTables(), $actual->getProdDatabase()->getIgnoredTables());
+        self::assertSame($expectedTestDatabase->getServer(), $actual->getTestDatabase()->getServer());
+        self::assertSame($expectedTestDatabase->getName(), $actual->getTestDatabase()->getName());
+        self::assertSame($expectedTestDatabase->getPort(), $actual->getTestDatabase()->getPort());
+        self::assertSame($expectedTestDatabase->getUsername(), $actual->getTestDatabase()->getUsername());
+        self::assertSame($expectedTestDatabase->getPassword(), $actual->getTestDatabase()->getPassword());
+        self::assertSame($expectedTestDatabase->getIgnoredTables(), $actual->getTestDatabase()->getIgnoredTables());
         self::assertSame($expectedFileServer->getServer(), $actual->getFileServer()->getServer());
         self::assertSame($expectedFileServer->getRootPath(), $actual->getFileServer()->getRootPath());
         self::assertSame($expectedFtp->getPort(), $actual->getFileServer()->getFtp()->getPort());
